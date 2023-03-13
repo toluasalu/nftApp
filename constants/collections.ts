3a6660102ec25c4eba59e8f5d3b41b754d247500
toluasalu <johnasalu13@gmail.com>
2023-03-13 06:01:12 +0100

    first commit

---
+++ constants/collections.ts
@@ -0,0 +1,25 @@
+const collections = [{
+    id: 1,
+    name:"Cyberpunk",
+    image: require("../assets/collections"),
+    price: "0.23",
+    creator: "Anna",
+    creatorImage: require("../assets/creators")
+},
+{
+    id: 2,
+    name: "Cyberpunk",
+    Image: require("../assets/collections"),
+    price: "1.23",
+    creator: "Lima",
+    creatorImage: require("../assets")
+},
+{
+    id: 3,
+    name:"Cyberpunk",
+    image: require("../assets/collections"),
+    price: "0.23",
+    creator: "Anna",
+    creatorImage: require("../assets/creators")
+}
+];
